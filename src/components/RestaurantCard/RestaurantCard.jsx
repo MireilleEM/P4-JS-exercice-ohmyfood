2360e955c6a15c596ff01aaf31038dcec47705b8
Mireille Emonot <130974267+MireilleEM@users.noreply.github.com>
2026-03-24 19:30:41 +0100

    coeur dégradé et Lien vers la page

--- src/components/RestaurantCard/RestaurantCard.jsx
+++ src/components/RestaurantCard/RestaurantCard.jsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link';
 import '@/app/styles.css';
 import styles from'./RestaurantCard.module.css'
 //import '@/app/pages.module.css'
@@ -8,6 +9,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { useState } from 'react';
 import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons"; 
+//import { useContext } from 'react'; // <-- NOUVEL IMPORT
+//import { FavoritesContext } from '@/context/FavoritesContext'; // <-- NOUVEL IMPORT
 
 
 //export default function RestaurantCard({ name, slug, location, image, isNew = false }) {
@@ -30,8 +33,8 @@ import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
   return (
     <>
       {restaurants.map((restaurant) => (
-  
-    <article key={restaurant.id} className={styles.card}>
+    <Link key={restaurant.id} href={`/restaurant/${restaurant.slug}`}>
+    <article  className={styles.card}>
       <div className={styles.imageContainer}>
         <img
           src= {restaurant.image}
@@ -52,19 +55,35 @@ import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
         <button 
           //onClick={() => setIsLiked(!isLiked)}
           //className={isLiked ? styles.favoriteButton : styles.notFavoriteButton}
-          onClick={() => upsertLike(restaurant.id)}
-          className={styles.favoriteButton}
+
+         // onClick={() => upsertLike(restaurant.id)} ne suffit pas, ça ouvre la page à chaque fois
+         className={styles.favoriteButton}
+          onClick={(e) => {
+          e.preventDefault();  // Stoppe le comportement par défaut
+          e.stopPropagation(); // NOUVEAU : Empêche le clic de remonter jusqu'au <Link> 
+          upsertLike(restaurant.id);
+        }}
         >
+
+{/* On crée le dégradé de façon invisible juste ici */}
+          <svg width="0" height="0" style={{ position: 'absolute' }}>
+            <linearGradient id="coeur-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+              <stop offset="0%" stopColor="#FF79DA" />
+              <stop offset="100%" stopColor="#9356DC" />
+            </linearGradient>
+          </svg>
+
           <FontAwesomeIcon
             //icon={isLiked ? faHeartSolid : faHeart}
             icon={likedIds.includes(restaurant.id) ? faHeartSolid : faHeart}
-            className={styles.heartIcon} 
+            className= {likedIds.includes(restaurant.id) ? styles.Liked : styles.heartIcon}
           />
         </button>
       </div>
 </article>
+</Link>
       ))}
-   </>
+</>   
   );
 };
 
